9b2e9b4266dcb88d4ee8d54132a6c56052881df2
Kunal Siyag <kunalsiyag@rediffmail.com>
2023-11-01 11:19:48 +0530

    new code

--- New_ROVERCODE/sketch_oct30a/sketch_oct30a.ino
+++ New_ROVERCODE/sketch_oct30a/sketch_oct30a.ino
@@ -1,11 +1,15 @@
 #include <WiFi.h>
 #include "ArduinoJson.h"
 #include <WebSocketClient.h>
+#include <DHT.h>
+#include <ESP32Servo.h>
+
+#define DHTPIN 27     // Digital pin connected to the DHT sensor
 const char* ssid = "Skyguy";
 const char* password = "TheForce";
 char path[] = "/";
-char host[] = "192.168.73.160:8082";
-String IP ="192.168.73.160";
+char host[] = "192.168.114.160:8082";
+String IP ="192.168.114.160";
 WebSocketClient webSocketClient;
 String Direction;
 // Use WiFiClient class to create TCP connections
@@ -23,13 +27,40 @@ const int freq= 30000;
 const int pwmChannel=0;
 const int resolution=8;
 int Speed=155;
-const int mq7Pin = 32; 
-const int mq4Pin = 34;
+const int mq7Pin = 33; 
+const int mq4Pin = 25;
+const int irsensor = 26;
 
+const int servoPin =23;
+
+#define DHTTYPE DHT11     // DHT 11
+int Angle=0;
+DHT dht(DHTPIN, DHTTYPE);
+//MQ7 Calibration
+float sensor_volt;
+float RS_gas; 
+float R0;
+int R2 = 2000;
+
+
+//MQ4 Calibration
+const int R_0 = 945; //Change this to your own R0 measurements
+
+
+// current temperature & humidity, updated in loop()
+float t = 0.0;
+float h = 0.0;
+
+
+
+  Servo myServo;
 void setup() {
   Serial.begin(115200);
+  dht.begin();
   delay(10);
 
+  myServo.attach(servoPin);
+  myServo.write(0);
 
   //Setting up motor driver and controls
   pinMode(motor1Pin1, OUTPUT);
@@ -72,7 +103,7 @@ void setup() {
   
 
   // Connect to the websocket server
-  if (client.connect("192.168.73.160", 8082)) {
+  if (client.connect("192.168.114.160", 8082)) {
     Serial.println("Connected");
   } else {
     Serial.println("Connection failed.");
@@ -109,7 +140,7 @@ void Reset_Motors()
 
 }
 
-void Forward()
+void Left()
 {
   ledcWrite(pwmChannel, Speed);
   digitalWrite(motor1Pin1,HIGH);
@@ -119,16 +150,7 @@ void Forward()
 
 }
 
-void Right()
-{
-  ledcWrite(pwmChannel, Speed);
-  digitalWrite(motor1Pin1,HIGH);
-  digitalWrite(motor1Pin2,LOW);
-  digitalWrite(motor2Pin1,LOW);
-  digitalWrite(motor2Pin2,HIGH);
-}
-
-void Left()
+void Backward()
 {
   ledcWrite(pwmChannel, Speed);
   digitalWrite(motor1Pin1,LOW);
@@ -137,7 +159,16 @@ void Left()
   digitalWrite(motor2Pin2,LOW);
 }
 
-void Backward()
+void Forward()
+{
+  ledcWrite(pwmChannel, Speed);
+  digitalWrite(motor1Pin1,HIGH);
+  digitalWrite(motor1Pin2,LOW);
+  digitalWrite(motor2Pin1,LOW);
+  digitalWrite(motor2Pin2,HIGH);
+}
+
+void Right()
 {
   ledcWrite(pwmChannel, Speed);
   digitalWrite(motor1Pin1,LOW);
@@ -161,7 +192,7 @@ void processReceivedJSON(String jsonStr) {
   Direction = doc["direction"].as<String>();
   Speed = doc["speed"].as<int>();
   Speed= map(Speed,0,100,0,255);
-  
+  Angle= doc["Angle"].as<int>();
 
   // Process the parsed data as needed
   Serial.println("Received JSON data:");
@@ -169,6 +200,7 @@ void processReceivedJSON(String jsonStr) {
   Serial.print(Direction);
   Serial.print(", key2: ");
   Serial.println(Speed);
+
   
 }
 
@@ -179,6 +211,8 @@ void loop() {
   String data;
   String data_send;
 
+  myServo.write(Angle);
+  int IR=analogRead(irsensor);
   if (client.connected()) {
     
     webSocketClient.getData(data);
@@ -189,7 +223,6 @@ void loop() {
     
 
     processReceivedJSON(data);
-    
     if (Direction=="up")
     {
       Forward();
@@ -216,18 +249,34 @@ void loop() {
       Reset_Motors();
       Serial.println("Stopped");
     }
+    
 
-
+    // Read temperature as Celsius (the default)
+    float newT = dht.readTemperature();
+    // Read temperature as Fahrenheit (isFahrenheit = true)
+    //float newT = dht.readTemperature(true);
+    // Read Humidity
+    float newH = dht.readHumidity();
 
 
 
     // Read sensor values
     int mq7Value = analogRead(mq7Pin);
+    sensor_volt=(float)mq7Value/1024*5.0;
+    RS_gas = ((5.0 * R2)/sensor_volt) - R2;
+    R0 = RS_gas / 1;
+
     int mq4Value = analogRead(mq4Pin);
+    float v_o = mq4Value * 5 / 1023; // convert reading to volts
+    float R_S = (5-v_o) * 1000 / v_o; // apply formula for getting RS
+    float PPM = pow(R_S/R_0,-2.95) * 1000; //apply formula for getting PPM
+
     // Create a JSON object to store the sensor data
     DynamicJsonDocument sensorData(128);
-    sensorData["mq7"] = mq7Value;
-    sensorData["mq4"] = mq4Value;
+    sensorData["mq7"] = R0;
+    sensorData["mq4"] = PPM;
+    sensorData["temperature"] = newT;
+    sensorData["Humidity"] = newH;
     
     // Convert the JSON object to a string
     serializeJson(sensorData, data_send);
@@ -237,7 +286,6 @@ void loop() {
     webSocketClient.sendData(data_send);
     Serial.println(data_send);
     // Delay or adjust as needed
-    delay(1000);
     
   } else {
     Serial.println("Client disconnected.");
